2a79fbead178a41d4b7890070083446a2a26651b
abk007 <itz.abk007@gmail.com>
2023-10-05 18:06:46 +0530

    Model Created for TimeSeries

---
+++ listener_service/model/TimeSeries.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const timeseriesSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    timestamp: {
+      type: Date,
+      required: true,
+    },
+    count: {
+      type: Number,
+      default: 0,
+    },
+    totalCount: {
+      type: Number,
+      default: 0,
+    },
+    data: [
+      {
+        origin: { type: String, required: true },
+        destination: { type: String, required: true },
+      },
+    ],
+  },
+  {
+    timeseries: {
+      timeField: "timestamp",
+      metaField: _id,
+      granularity: "minutes",
+    },
+  }
+);
+
+const TimeSeries = mongoose.model("TimeSeries", timeseriesSchema);
+
+module.exports = TimeSeries;
