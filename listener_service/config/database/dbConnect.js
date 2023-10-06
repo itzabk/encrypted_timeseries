b8256081ef57782fbfc1b4e3fe55cee21e3d7bc3
abk007 <itz.abk007@gmail.com>
2023-10-06 09:33:10 +0530

    Remodified dbconn

--- listener_service/config/database/dbConnect.js
+++ listener_service/config/database/dbConnect.js
@@ -5,14 +5,6 @@ const dbConnect = async () => {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      server: {
-        auto_reconnect: true,
-        socketOptions: {
-          connectTimeoutMS: 3600000,
-          keepAlive: 3600000,
-          socketTimeoutMS: 3600000,
-        },
-      },
     });
     if (conn) {
       console.log(
